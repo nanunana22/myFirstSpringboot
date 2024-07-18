74ac69b81c7a6aa0b68d689742341362e888964c
ursulakohler <kohlerursula22@gmail.com>
2024-07-18 12:32:42 +0200

    add endpoint GetMapping

--- src/main/java/org/example/myfirstspringboot/MessageControler.java
+++ src/main/java/org/example/myfirstspringboot/MessageControler.java
@@ -1,9 +1,6 @@
 package org.example.myfirstspringboot;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +15,12 @@ public class MessageControler {
     @PostMapping
     public String sendMessage(@RequestBody Message message) {
         messages.add(message);
-        System.out.println(messages);
         return "Nachricht erfolgreich versendet";
+    }
+
+    @GetMapping
+    public List<Message> getMessages() {
+        return messages;
     }
 
 
