6afb284626b6ed4849213823b1a4c41a83479747
ursulakohler <kohlerursula22@gmail.com>
2024-07-18 10:06:50 +0200

    new

--- src/main/java/org/example/myfirstspringboot/HelloWorldController.java
+++ src/main/java/org/example/myfirstspringboot/HelloWorldController.java
@@ -1,2 +1,17 @@
-package org.example.myfirstspringboot;public class HelloWorldController {
+package org.example.myfirstspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+
 }
--- src/test/java/org/example/myfirstspringboot/MyFirstSpringbootApplicationTests.java
+++ src/test/java/org/example/myfirstspringboot/MyFirstSpringbootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyFirstSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
