99a7ee1911406310146de8e0417293f76978c1a4
ursulakohler <kohlerursula22@gmail.com>
2024-07-18 10:06:42 +0200

    Initial commit

---
+++ src/main/java/org/example/myfirstspringboot/HelloWorldController.java
@@ -0,0 +1,2 @@
+package org.example.myfirstspringboot;public class HelloWorldController {
+}
---
+++ src/main/java/org/example/myfirstspringboot/MyFirstSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.myfirstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringbootApplication.class, args);
+	}
+
+}
